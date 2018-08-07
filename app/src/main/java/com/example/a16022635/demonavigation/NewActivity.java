50e75a763000b4971ee870195f6a9134365611b8
teozhirong1998 <16022635@myrp.edu.sg>
2018-08-07 17:36:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022635/demonavigation/NewActivity.java
@@ -0,0 +1,30 @@
+package com.example.a16022635.demonavigation;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class NewActivity extends AppCompatActivity {
+
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        ab=getSupportActionBar();
+        ab.setTitle("");
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //action
+            }
+        });
+    }
+}
